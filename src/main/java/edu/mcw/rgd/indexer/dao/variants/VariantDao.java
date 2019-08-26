509a0932f98101879b306e396b8d299ca78607b4
jt15 <jthota01@gmail.com>
2019-08-26 11:21:07 -0500

    code cleanup

--- src/main/java/edu/mcw/rgd/indexer/dao/variants/VariantDao.java
+++ src/main/java/edu/mcw/rgd/indexer/dao/variants/VariantDao.java
@@ -201,9 +201,23 @@ public class VariantDao extends VariantDAO {
                 }
             }
 
+                rs.close();
+                stmt.close();
+               connection.close();
 
         } catch (Exception e) {
             e.printStackTrace();
+            try {
+                if(rs!=null)
+                    rs.close();
+                if(stmt!=null)
+                    stmt.close();
+                if(connection!=null)
+                    connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+
         }finally {
              try {
                     if(rs!=null)
