43ba7558a7c6398a2ed76af8f38dbcd4eb7f1d59
Thota <jthota@mcw.edu>
2025-10-31 12:32:50 -0500

    updated gene expression tags

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -4,11 +4,11 @@ APPDIR=/home/rgddata/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAIL_LIST=jthota@mcw.edu
 if [ "$SERVER" = "REED" ]; then
-  EMAIL_LIST=mtutaj@mcw.edu,jthota@mcw.edu,jdepons@mcw.edu
+  EMAIL_LIST="mtutaj@mcw.edu jthota@mcw.edu jdepons@mcw.edu"
 fi
 cd $APPDIR
 pwd
-DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
+DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db2.xml"
 
 
 #LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
