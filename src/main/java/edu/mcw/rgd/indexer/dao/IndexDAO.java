51088df3a7dd7919da6c0fd74998b92da9ad15be
Thota <jthota@mcw.edu>
2026-06-18 14:34:49 -0500

    fixed missin HP

--- src/main/java/edu/mcw/rgd/indexer/dao/IndexDAO.java
+++ src/main/java/edu/mcw/rgd/indexer/dao/IndexDAO.java
@@ -681,7 +681,7 @@ public class IndexDAO extends AbstractDAO {
 
     public void getGViewer() throws Exception {
         List<Integer> speciesTypeKeys=Arrays.asList(3,2,1);
-        List<String> ontIds = Arrays.asList("CC", "MF", "BP", "RDO", "PW", "NBO", "MP", "CMO", "MMO", "XCO", "VT", "CHEBI", "RS");
+        List<String> ontIds = Arrays.asList("CC", "MF", "BP", "RDO", "PW", "NBO", "MP", "CMO", "MMO", "XCO", "VT", "CHEBI", "RS", "HP");
 
 
         ExecutorService executor = new MyThreadPoolExecutor(10, 10, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
